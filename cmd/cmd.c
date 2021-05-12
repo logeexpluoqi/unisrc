b8ac983a3dcf9a11a63e80ff084cfaa5e95f2841
luoqi <logeexpluoqi@outlook.com>
2021-05-12 18:08:55 +0800

    [finish cmd arch]

--- cmd/cmd.c
+++ cmd/cmd.c
@@ -2,40 +2,92 @@
  * @Author: luoqi 
  * @Date: 2021-04-29 00:29:54 
  * @Last Modified by: luoqi
- * @Last Modified time: 2021-04-29 19:54:34
+ * @Last Modified time: 2021-05-12 18:04:00
  */
 
 #include "./cmd.h"
 
+#define ASCII_SPACE 0x20
 static LIST_HEAD(cmd_list);
 
-unsigned char cmd_exec(CmdObj* obj, int argc, const char* argv[])
+static unsigned int cmd_id = 1;
+
+static unsigned char cmd_strcmp(const char* s1, const char* s2);
+
+CmdErrType cmd_exec(char* cmd_msg)
 {
     ListObj* node;
     CmdObj* cmd;
-    unsigned char state = 0;
+    unsigned int argc;
+    char* argv[CMD_MAX_NUM + 1];
+
+    /* This section is used to dismantling input command string */
+    unsigned char i = 0;
+    unsigned int argn = 0;
+
+    while (cmd_msg[i] != 0)
+    {
+        if (i > CMD_MAX_LEN - 1) {
+            return CMD_LEN_OUT;
+        }
+        else if (argn > CMD_MAX_NUM - 1) {
+            return CMD_NUM_OUT;
+        }
+
+        if (i == 0)
+        {
+            argv[argn] = &cmd_msg[i];
+        }
+        if (cmd_msg[i] == ASCII_SPACE)
+        {
+            argv[++argn] = &cmd_msg[i + 1];
+            cmd_msg[i] = 0;
+        }
+        i++;
+    }
+    argc = argn + 1; // add 1 because there is no space key value front of the first argv
+    /* End of dismantling input command string */
 
     list_for_each(node, &cmd_list)
     {
         cmd = list_entry(node, CmdObj, cmd_list);
-        if(obj->id == cmd->id)
+        if (cmd_strcmp(cmd->name, argv[0]) == 0)
         {
-            state = cmd->cmd_hdl(argc, argv);
+            if (cmd->param_num > argc - 1)
+            {
+                return PARAM_LITTLE;
+            }
+            else if (cmd->param_num < argc - 1)
+            {
+                return PARAM_EXCEED;
+            }
+
+            if (cmd->cmd_hdl(argc, argv) != 0)
+            {
+                return CMD_EXEC_ERR;
+            }
+            else
+            {
+                return NO_ERR;
+            }
         }
     }
-    return state;
+
+    return NO_CMD;
 }
 
-void cmd_init(CmdObj* obj, 
-              char* name,
-              unsigned int id,
-              unsigned char(*cmd_hdl)(int, const char* []), 
-              char* usage)
+void cmd_init(CmdObj* obj,
+    const char* name,
+    unsigned char param_num,
+    unsigned char(*cmd_hdl)(int, char* []),
+    const char* usage)
 {
     obj->name = name;
-    obj->id = id;
+    obj->param_num = param_num;
+    obj->id = cmd_id;
     obj->cmd_hdl = cmd_hdl;
     obj->usage = usage;
+    cmd_id++;
 }
 
 void cmd_add(CmdObj* obj)
@@ -43,17 +95,11 @@ void cmd_add(CmdObj* obj)
     list_insert_before(&cmd_list, &obj->cmd_list);
 }
 
-unsigned char cmd_del(CmdObj* obj)
+void cmd_del(CmdObj* obj)
 {
-    if(cmd_isexist(obj))
-    {
-        list_remove(&obj->cmd_list);
-        return 0;
-    }
-    else 
-        return 1;
+    list_remove(&obj->cmd_list);
 }
- 
+
 unsigned char cmd_isexist(CmdObj* obj)
 {
     unsigned char isexist = 0;
@@ -63,7 +109,7 @@ unsigned char cmd_isexist(CmdObj* obj)
     list_for_each(node, &cmd_list)
     {
         cmd = list_entry(node, CmdObj, cmd_list);
-        if(obj->id == cmd->id)
+        if (obj->id == cmd->id)
         {
             isexist = 1;
         }
@@ -72,8 +118,33 @@ unsigned char cmd_isexist(CmdObj* obj)
     return isexist;
 }
 
+unsigned int cmd_get_id(CmdObj* obj)
+{
+    return obj->id;
+}
+
 unsigned int cmd_num()
 {
     return list_len(&cmd_list);
 }
 
+void cmd_help()
+{
+
+}
+
+unsigned char cmd_strcmp(const char* s1, const char* s2)
+{
+    unsigned int i = 0;
+
+    while (s1[i] != 0 || s2[i] != 0)
+    {
+        if (((s1[i] == 0) && (s2[i] != 0)) || ((s1[i] != 0) && (s2[i] == 0)) || (s1[i] != s2[i]))
+        {
+            return 1;
+        }
+        i++;
+    }
+
+    return 0;
+}
--- cmd/cmd.h
+++ cmd/cmd.h
@@ -2,32 +2,51 @@
  * @Author: luoqi 
  * @Date: 2021-04-29 00:30:20 
  * @Last Modified by: luoqi
- * @Last Modified time: 2021-04-29 19:53:28
+ * @Last Modified time: 2021-05-12 14:33:40
  */
 
 #ifndef _CMD_H
 #define _CMD_H
 
 #include "../common/list.h"
+#define CMD_MAX_LEN     60
+#define CMD_MAX_NUM     20
+
+typedef enum cmd_err
+{
+    NO_ERR = 0,
+    CMD_LEN_OUT,
+    CMD_NUM_OUT,
+    NO_CMD,
+    PARAM_EXCEED,
+    PARAM_LITTLE,
+    CMD_EXEC_ERR
+} CmdErrType;
+
 typedef struct cmd_object
 {
-    char* name;
+    const char* name;
+    /* id auto increase from 1, if return id is 0 means that there is not target command */
     unsigned int id;
-    unsigned char (*cmd_hdl)(int, const char* []);
-    char* usage; 
+    /* parameter number, max 255 */
+    unsigned char param_num;
+    unsigned char (*cmd_hdl)(int, char* []);
+    const char* usage;
     ListObj cmd_list;
 } CmdObj;
 
-unsigned char cmd_exec(CmdObj* obj, int argc, const char* argv[]);
-void cmd_init(CmdObj* obj, 
-              char* name,
-              unsigned int id,
-              unsigned char(*cmd_hdl)(int, const char* []), 
-              char* usage
-             );
+CmdErrType cmd_exec(char* cmd_msg);
+void cmd_init(CmdObj* obj,
+    const char* name,
+    unsigned char param_num,
+    unsigned char(*cmd_hdl)(int, char* []),
+    const char* usage);
 void cmd_add(CmdObj* obj);
-unsigned char cmd_del(CmdObj* obj);
+void cmd_del(CmdObj* obj);
 unsigned char cmd_isexist(CmdObj* obj);
+unsigned int cmd_get_id(CmdObj* obj);
 unsigned int cmd_num(void);
+
+void cmd_help(void);
 
 #endif
