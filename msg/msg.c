7fc3725d606f503f5a79d00b924e212555d72729
luoqi <logeexpluoqi@outlook.com>
2021-06-09 14:45:36 +0800

    feat(msg): add msg arc

---
+++ msg/msg.c
@@ -0,0 +1,162 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-05-11 10:02:21 
+ * @Last Modified by:   luoqi 
+ * @Last Modified time: 2021-05-11 10:02:21 
+ */
+
+#include "msg.h"
+
+#define NULL (void*) 0
+
+static unsigned char msg_strcmp(const char* s1, const char* s2);
+static unsigned int msg_container_num = 0;
+
+void msg_container_init(MsgContainer* container, const char* name, const char* usage)
+{
+    container->name = name;
+    container->usage = usage;
+    list_init(&container->msg_list);
+    msg_container_num++;
+}
+
+void msg_init(MsgObj* obj,
+    const char* name,
+    void* mem,
+    MsgType type,
+    const char* usage)
+{
+    obj->name = name;
+    obj->mem = mem;
+    obj->type = type;
+    obj->msg_state = MSG_UNLOCK;
+    obj->usage = usage;
+}
+
+void msg_add(MsgContainer* container, MsgObj* obj)
+{
+    list_insert_before(&container->msg_list, &obj->msg_list_node);
+}
+
+void msg_del(MsgObj* obj)
+{
+    list_remove(&obj->msg_list_node);
+}
+
+void* msg_get(MsgContainer* container, const char* name)
+{
+    ListObj* node;
+    MsgObj* msg;
+
+    list_for_each(node, &container->msg_list)
+    {
+        msg = list_entry(node, MsgObj, msg_list_node);
+        if (msg_strcmp(name, msg->name) == 0 && msg->msg_state == MSG_UNLOCK)
+            return msg->mem;
+    }
+    return NULL;
+}
+
+unsigned char msg_set(MsgContainer* container, const char* name, void* msg_data, unsigned int size)
+{
+    unsigned int i;
+    ListObj* node;
+    MsgObj* msg;
+
+    list_for_each(node, &container->msg_list)
+    {
+        msg = list_entry(node, MsgObj, msg_list_node);
+        msg->msg_state = MSG_LOCKED;
+        if (msg_strcmp(name, msg->name) == 0)
+        {
+            switch (msg->type)
+            {
+            case MSG_CHAR: 
+                for(i = 0; i < size; i ++)
+                    ((char*)msg->mem)[i] = ((char*)msg_data)[i];
+                break;
+            case MSG_UCHAR: 
+                for(i = 0; i < size; i ++)
+                    ((unsigned char*)msg->mem)[i] = ((unsigned char*)msg_data)[i];
+                break;
+            case MSG_INT: 
+                for(i = 0; i < size; i ++)
+                    ((int*)msg->mem)[i] = ((int*)msg_data)[i];
+                break;
+            case MSG_UINT: 
+                for(i = 0; i < size; i ++)
+                    ((unsigned int*)msg->mem)[i] = ((unsigned int*)msg_data)[i];
+                break;
+            case MSG_FLOAT: 
+                for(i = 0; i < size; i ++)
+                    ((float*)msg->mem)[i] = ((float*)msg_data)[i];
+                break;
+            case MSG_DOUBLE: 
+                for(i = 0; i < size; i ++)
+                    ((double*)msg->mem)[i] = ((double*)msg_data)[i];
+                break;
+            case MSG_LINT: 
+                for(i = 0; i < size; i ++)
+                    ((long int*)msg->mem)[i] = ((long int*)msg_data)[i];
+                break;
+            case MSG_LLINT: 
+                for(i = 0; i < size; i ++)
+                    ((long long int*)msg->mem)[i] = ((long long int*)msg_data)[i];
+                break;
+            }
+            msg->msg_state = MSG_UNLOCK;
+        }
+    }
+    return 0;
+}
+
+unsigned char msg_mem_location_set(MsgContainer* container, const char* name, void* new_mem)
+{
+    ListObj* node;
+    MsgObj* msg;
+
+    list_for_each(node, &container->msg_list)
+    {
+        msg = list_entry(node, MsgObj, msg_list_node);
+        msg->msg_state = MSG_LOCKED;
+        if (msg_strcmp(name, msg->name) == 0)
+        {
+            msg->mem = new_mem;
+            msg->msg_state = MSG_UNLOCK;
+        }
+    }
+    return 0;
+}
+
+unsigned int msg_container_num_get()
+{
+    return msg_container_num;
+}
+
+unsigned int msg_num_get(MsgContainer* container)
+{
+    return list_len(&container->msg_list);
+}
+
+MsgObj* msg_obj_get(MsgContainer* container, unsigned int serial)
+{
+    ListObj* node = &container->msg_list;
+    for (unsigned int i = serial; i > 0; i--)
+        node = node->next;
+    return list_entry(node, MsgObj, msg_list_node);
+}
+
+static unsigned char msg_strcmp(const char* s1, const char* s2)
+{
+    unsigned int i = 0;
+
+    while (s1[i] != 0 || s2[i] != 0)
+    {
+        if (((s1[i] == 0) && (s2[i] != 0)) || ((s1[i] != 0) && (s2[i] == 0)) || (s1[i] != s2[i]))
+        {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
---
+++ msg/msg.h
@@ -0,0 +1,69 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-05-11 10:02:35 
+ * @Last Modified by:   luoqi 
+ * @Last Modified time: 2021-05-11 10:02:35 
+ */
+#ifndef _MSG_H
+#define _MSG_H
+
+#include "../common/list.h"
+
+typedef enum MSG_STATE
+{
+    MSG_LOCKED,
+    MSG_UNLOCK
+} MsgState;
+
+typedef enum MSG_TYPE
+{
+    MSG_CHAR,
+    MSG_UCHAR,
+    MSG_INT,
+    MSG_UINT,
+    MSG_FLOAT,
+    MSG_DOUBLE,
+    MSG_LINT,
+    MSG_LLINT
+} MsgType;
+
+typedef struct msg_container
+{
+    const char* name;
+    const char* usage;
+    ListObj msg_list;
+} MsgContainer;
+
+typedef struct msg_obj
+{
+    const char* name;
+    void* mem;
+    MsgState msg_state;
+    MsgType type;
+    const char* usage;
+    ListObj msg_list_node;
+} MsgObj;
+
+#define MSG_CREAT(name, mem, type, size)     MsgObj name; type mem[size]
+#define MSG_SIZE(mem, type)                 (sizeof(mem)) / (sizeof(type))
+
+void msg_container_init(MsgContainer* container,
+                        const char* name,
+                        const char* usage);
+
+void msg_init(MsgObj* obj,
+              const char* name,
+              void* mem,
+              MsgType type,
+              const char* usage);
+
+void msg_add(MsgContainer* container, MsgObj* obj);
+void msg_del(MsgObj* obj);
+void* msg_get(MsgContainer* container, const char* name);
+unsigned char msg_set(MsgContainer* container, const char* name, void* msg_data, unsigned int size);
+unsigned char msg_mem_location_set(MsgContainer* container, const char* name, void* new_mem);
+unsigned int msg_container_num_get(void);
+unsigned int msg_num_get(MsgContainer* container);
+MsgObj* msg_obj_get(MsgContainer* container, unsigned int serial);
+
+#endif
