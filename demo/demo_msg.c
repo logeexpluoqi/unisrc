2b5787264f3da7df5518ea97a5544b3e55907794
luoqi <logeexpluoqi@outlook.com>
2021-09-14 13:35:43 +0800

    code(*): add msg test demo file add optimized cmake file

---
+++ demo/demo_msg.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-14 12:09:37 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2021-09-14 12:13:27
+ */
+
+#include "demo_msg.h"
+#include "../kernel/msg.h"
+
+int demo_msg_init()
+{
+
+    return 0;
+}
---
+++ demo/demo_msg.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-14 12:09:51 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2021-09-14 12:12:50
+ */
+
+#ifndef _DEMO_MSG_H
+#define _DEMO_MSG_H
+
+int demo_msg_init(void);
+
+#endif
