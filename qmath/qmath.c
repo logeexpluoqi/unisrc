5e0e9d42d4843b463e3c77e2669363d5615727ba
luoqi <logeexpluoqi@outlook.com>
2021-09-17 11:09:09 +0800

    file(qmath): add qmath files used to math lib

---
+++ qmath/qmath.c
@@ -0,0 +1,10 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-17 11:03:50 
+ * @Last Modified by:   luoqi 
+ * @Last Modified time: 2021-09-17 11:03:50 
+ */
+
+#include "qmath.h"
+
+
---
+++ qmath/qmath.h
@@ -0,0 +1,11 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-17 11:02:33 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2021-09-17 11:03:26
+ */
+
+#ifndef _QMATH_H
+#define _QMATH_H
+
+#endif
