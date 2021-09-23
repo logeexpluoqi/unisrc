873b570be6615fa791a81f065022530017a9a705
luoqi <logeexpluoqi@outlook.com>
2021-09-23 14:49:50 +0800

    file(qmath): add qtensor files

---
+++ qmath/qtensor.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-23 14:49:05 
+ * @Last Modified by:   luoqi 
+ * @Last Modified time: 2021-09-23 14:49:05 
+ */
+
+#include "qtensor.h"
---
+++ qmath/qtensor.h
@@ -0,0 +1,11 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2021-09-23 14:48:27 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2021-09-23 14:49:03
+ */
+
+#ifndef _QTENSOR_H
+#define _QTENSOR_H
+
+#endif
