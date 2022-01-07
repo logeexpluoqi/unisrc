cd077ceebfdd9e91e70aef6e2a59aa3e313e526a
luoqi <logeexpluoqi@outlook.com>
2022-01-07 10:14:23 +0800

    file(demo): add qkey demo file

---
+++ demo/demo_qkey.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-01-07 10:10:01 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-01-07 10:12:31
+ */
+
+#include "demo_qkey.h"
+
+int demo_qkey_init()
+{
+
+    return 0;
+}
---
+++ demo/demo_qkey.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-01-07 10:10:37 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-01-07 10:12:14
+ */
+
+#ifndef _DEMO_QKEY_H
+#define _DEMO_QKEY_H
+
+int demo_qkey_init(void);
+
+#endif
