a0df12e1b9f3a6c467ce587c8f3350cbb77966cb
luoqi <logeexpluoqi@outlook.com>
2022-01-26 16:05:47 +0800

    feat(qmath): add some function

--- qmath/qmath.c
+++ qmath/qmath.c
@@ -1,8 +1,8 @@
 /*
  * @Author: luoqi 
  * @Date: 2021-09-17 11:03:50 
- * @Last Modified by:   luoqi 
- * @Last Modified time: 2021-09-17 11:03:50 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-01-26 14:26:20
  */
 
 #include "qmath.h"
@@ -17,4 +17,30 @@ float qabs(float x)
 float qsign(float x)
 {
     return (x >= 0.0 ? 1.0 : -1.0);
+}
+
+float qmax(float a, float b)
+{
+    if(a >= b)
+        return a;
+    else 
+        return b;
+}
+
+float qmin(float a, float b)
+{
+    if(a <= b)
+        return a;
+    else
+        return b;
+}
+
+float qfloor(float x)
+{
+    return (float)((int)(x));
+}
+
+float qceil(float x)
+{
+    return (float)((int)(x + 1));
 }
--- qmath/qmath.h
+++ qmath/qmath.h
@@ -2,13 +2,22 @@
  * @Author: luoqi 
  * @Date: 2021-09-17 11:02:33 
  * @Last Modified by: luoqi
- * @Last Modified time: 2021-09-17 11:03:26
+ * @Last Modified time: 2022-01-26 14:24:34
  */
 
 #ifndef _QMATH_H
 #define _QMATH_H
 
 float qabs(float x);
+
 float qsign(float x);
+
+float qmax(float a, float b);
+
+float qmin(float a, float b);
+
+float qfloor(float x);
+
+float qceil(float x);
 
 #endif
