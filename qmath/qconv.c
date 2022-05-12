70a82feccae3d2def1143cc4622568885ca84523
luoqi <logeexpluoqi@outlook.com>
2022-05-12 21:05:21 +0800

    files(qmath): add qconv files

---
+++ qmath/qconv.c
@@ -0,0 +1,6 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-04-30 22:01:23 
+ * @Last Modified by:   luoqi 
+ * @Last Modified time: 2022-04-30 22:01:23 
+ */
---
+++ qmath/qconv.h
@@ -0,0 +1,20 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-04-30 22:01:27 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-04-30 22:03:11
+ */
+
+#ifndef _QCONV_H
+#define _QCONV_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
