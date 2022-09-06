30ed264b5160620781bb0bd2bfe9c303bfc6e2e7
luoqi <logeexpluoqi@outlook.com>
2022-09-06 11:24:32 +0800

    code(demo): add mthread demo

--- demo/demo_mthread.c
+++ demo/demo_mthread.c
@@ -8,9 +8,23 @@
 #include "demo_mthread.h"
 #include "../linuxbase/mthread.h"
 
+static MThread thread;
+static void *thread_hdl(void *args);
+
 int demo_mthread_init()
 {
     mthread_basic_init();
-
+    mthread_init(&thread, "mthread_demo", 50, 500000, thread_hdl, "mthread demo");
+    mthread_start(&thread);
     return 0;
+}
+
+void *thread_hdl(void *args)
+{
+    for(;;){
+        mthread_task_begin(&thread);
+
+
+        mthread_task_end(&thread);
+    }
 }
