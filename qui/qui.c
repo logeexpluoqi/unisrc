b333017966b103cb391c2ed6d19c8045321595ae
luoqi <logeexpluoqi@outlook.com>
2022-11-08 21:39:09 +0800

    file(*): add some next

---
+++ qui/qui.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-11-08 20:38:40 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-11-08 20:39:49
+ */
+
+#include "qui.h"
---
+++ qui/qui.h
@@ -0,0 +1,29 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2022-11-08 20:38:50 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2022-11-08 20:42:54
+ */
+
+#ifndef _QUI_H
+#define _QUI_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+typedef struct 
+{
+    int size_x;
+    int size_y; 
+} QuiObj;
+
+int qui_init(QuiObj *ui, int size_x, int size_y);
+
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
