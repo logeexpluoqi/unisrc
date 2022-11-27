da63a2a8bacb268109dbcfba973dfa8eedfc926d
luoqi <logeexpluoqi@outlook.com>
2022-11-27 22:34:55 +0800

    fix(solver): fixed euler solver bug

--- solver/ode_euler.c
+++ solver/ode_euler.c
@@ -2,31 +2,31 @@
  * @Author: luoqi 
  * @Date: 2022-01-26 14:45:52 
  * @Last Modified by: luoqi
- * @Last Modified time: 2022-11-27 21:58:00
+ * @Last Modified time: 2022-11-27 22:32:55
  */
 
 #include "ode_euler.h"
 
-int ode_euler_1st_calcu(float *y, float (*dy)(float _x, float _y), float h, float y0, float t0, int len)
+int ode_euler_1st_calcu(float *y, float (*dy)(float _x, float _y), float h, float x0, float y0, int len)
 {
-    float tn;
+    float xn;
     if(len < 0){
         return -1;
     }
     y[0] = y0;
     for(int i = 0; i < (len - 1); i ++){
-        tn = i * h + t0;
-        y[i+1] = y[i] + h * dy(tn, y[i]);
+        xn = i * h + x0;
+        y[i+1] = y[i] + h * dy(xn, y[i]);
     }
     return 0;
 }
 
-int ode_euler_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float y0, float h)
+int ode_euler_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float h, float x0, float y0)
 {
     solver->dy = dy;
     solver->h = h;
     solver->y = y0;
-    solver->x = 0;
+    solver->x = x0;
     return 0;
 }
 
@@ -38,18 +38,18 @@ float ode_euler_1st_k_calcu(OdeEuer *solver)
     return y;
 }
 
-int ode_euler_2st_calcu(float *y, float (*dy)(float _x, float _y), float h, float y0, float t0, int len)
+int ode_euler_2st_calcu(float *y, float (*dy)(float _x, float _y), float h, float x0, float y0, int len)
 {
     float y_hat;
-    float tn;
+    float xn;
     if(len < 0){
         return -1;
     }
     y[0] = y0;
     for(int i = 0; i < (len - 1); i ++){
-        tn = i * h + t0;
-        y_hat = y[i] + h * dy(tn, y[i]);
-        y[i+1] = y[i] + (h / 2) * (dy(tn, y[i]) + dy(tn + h, y_hat));
+        xn = i * h + x0;
+        y_hat = y[i] + h * dy(xn, y[i]);
+        y[i+1] = y[i] + (h / 2) * (dy(xn, y[i]) + dy(xn + h, y_hat));
     }
     return 0;
 }
@@ -57,7 +57,7 @@ int ode_euler_2st_calcu(float *y, float (*dy)(float _x, float _y), float h, floa
 float ode_euler_2st_k_calcu(OdeEuer *solver)
 {
     float y_hat = solver->y + solver->h * solver->dy(solver->x, solver->y);
-    float y = solver->y + (solver->h / 2) * (solver->dy(solver->x, solver->y) + solver->dy(solver->x, y_hat));
+    float y = solver->y + (solver->h / 2) * (solver->dy(solver->x, solver->y) + solver->dy(solver->x + solver->h, y_hat));
     solver->y = y;
     solver->x = solver->x + solver->h;
     return y;
--- solver/ode_euler.h
+++ solver/ode_euler.h
@@ -2,7 +2,7 @@
  * @Author: luoqi 
  * @Date: 2021-09-17 11:13:12 
  * @Last Modified by: luoqi
- * @Last Modified time: 2022-11-27 21:51:57
+ * @Last Modified time: 2022-11-27 22:33:34
  */
 
 #ifndef _ODE_EULER_H
@@ -20,17 +20,17 @@ typedef struct
     float h;
 } OdeEuer;
 
-int ode_euler_1st_calcu(float *y, float (*dy)(float _x, float _y), float h, float y0, float t0, int len);
+int ode_euler_1st_calcu(float *y, float (*dy)(float _x, float _y), float h, float x0, float y0, int len);
 
-int ode_euler_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float y0, float h);
+int ode_euler_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float x0, float y0, float h);
 
 float ode_euler_1st_k_calcu(OdeEuer *solver);
 
 /* prediction-correction formula */
 
-int ode_euler_2st_calcu(float *y, float (*dy)(float _x, float _y), float h, float y0, float t0, int len);
+int ode_euler_2st_calcu(float *y, float (*dy)(float _x, float _y), float h, float x0, float y0, int len);
 
-int ode_euler_2st_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float y0, float h);
+int ode_euler_2st_k_init(OdeEuer *solver, float (*dy)(float _x, float _y), float h, float x0, float y0);
 
 float ode_euler_2st_k_calcu(OdeEuer *solver);
 
