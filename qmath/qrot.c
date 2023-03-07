9e6447c964af491b78b056c5eca0f521eb8ff271
luoqi <logeexpluoqi@outlook.com>
2023-03-07 22:01:14 +0800

    code(qrot): fixed some bug

--- qmath/qrot.c
+++ qmath/qrot.c
@@ -2,24 +2,29 @@
  * @Author: luoqi 
  * @Date: 2023-03-07 20:38:56 
  * @Last Modified by: luoqi
- * @Last Modified time: 2023-03-07 21:41:11
+ * @Last Modified time: 2023-03-07 22:00:00
  */
 
 #include <math.h>
 #include "qrot.h"
 
+#define qrot_sin(x)         sinf(x)
+#define qrot_cos(x)         cosf(x)
+#define qrot_atan2(y, x)    atan2f(y, x)
+#define qrot_asin(x)        asinf(x)
+
 QRotCoord3D qrot_euler(QRotCoord3D c, QRotEuler e)
 {
     QRotCoord3D _c;
-    _c.x = c.x * (cosf(e.alpha) * cosf(e.gamma) - sinf(e.alpha) * cosf(e.beta) * sinf(e.gamma)) 
-           + c.y * (-sinf(e.alpha) * cosf(e.gamma) - cosf(e.alpha) * cosf(e.beta) * sinf(e.gamma))
-           + c.z * (sinf(e.beta) * sinf(e.gamma));
-    _c.y = c.x * (cosf(e.alpha) * sinf(e.gamma) + sinf(e.alpha) * cosf(e.beta) * cosf(e.gamma)) 
-           + c.y * (-sinf(e.alpha) * sinf(e.gamma) + cosf(e.alpha) * cosf(e.beta) * cosf(e.gamma))
-           + c.z * (-sinf(e.beta) * cosf(e.gamma));
-    _c.z = c.x * (sinf(e.alpha) * sinf(e.beta))
-           + c.y * (cosf(e.alpha) * sinf(e.beta))
-           + c.z * cosf(e.beta);
+    _c.x = c.x * (qrot_cos(e.alpha) * qrot_cos(e.gamma) - qrot_sin(e.alpha) * qrot_cos(e.beta) * qrot_sin(e.gamma)) 
+           + c.y * (-qrot_sin(e.alpha) * qrot_cos(e.gamma) - qrot_cos(e.alpha) * qrot_cos(e.beta) * qrot_sin(e.gamma))
+           + c.z * (qrot_sin(e.beta) * qrot_sin(e.gamma));
+    _c.y = c.x * (qrot_cos(e.alpha) * qrot_sin(e.gamma) + qrot_sin(e.alpha) * qrot_cos(e.beta) * qrot_cos(e.gamma)) 
+           + c.y * (-qrot_sin(e.alpha) * qrot_sin(e.gamma) + qrot_cos(e.alpha) * qrot_cos(e.beta) * qrot_cos(e.gamma))
+           + c.z * (-qrot_sin(e.beta) * qrot_cos(e.gamma));
+    _c.z = c.x * (qrot_sin(e.alpha) * qrot_sin(e.beta))
+           + c.y * (qrot_cos(e.alpha) * qrot_sin(e.beta))
+           + c.z * qrot_cos(e.beta);
 
     return _c;
 }
@@ -37,10 +42,10 @@ QRotQuat qrot_e2q(QRotEuler e)
     float alpha = e.alpha / 2;
     float beta = e.beta/ 2;
     float gamma = e.gamma / 2;
-    q.w = cosf(alpha) * cosf(beta) * cosf(gamma) + sinf(alpha) * sinf(beta) * sinf(gamma);
-    q.i = sinf(alpha) * cosf(beta) * cosf(gamma) - cosf(alpha) * sinf(beta) * sinf(gamma);
-    q.j = cosf(alpha) * sinf(beta) * cosf(gamma) + sinf(alpha) * cosf(beta) * sinf(gamma);
-    q.k = cosf(alpha) * cosf(beta) * sinf(gamma) - sinf(alpha) * sinf(beta) * cosf(gamma);
+    q.w = qrot_cos(alpha) * qrot_cos(beta) * qrot_cos(gamma) + qrot_sin(alpha) * qrot_sin(beta) * qrot_sin(gamma);
+    q.i = qrot_sin(alpha) * qrot_cos(beta) * qrot_cos(gamma) - qrot_cos(alpha) * qrot_sin(beta) * qrot_sin(gamma);
+    q.j = qrot_cos(alpha) * qrot_sin(beta) * qrot_cos(gamma) + qrot_sin(alpha) * qrot_cos(beta) * qrot_sin(gamma);
+    q.k = qrot_cos(alpha) * qrot_cos(beta) * qrot_sin(gamma) - qrot_sin(alpha) * qrot_sin(beta) * qrot_cos(gamma);
 
     return q;
 }
@@ -48,9 +53,9 @@ QRotQuat qrot_e2q(QRotEuler e)
 QRotEuler qrot_q2e(QRotQuat q)
 {
     QRotEuler e;
-    e.alpha = atan2f(2 * (q.w * q.i + q.j * q.k), 1 - 2 * (q.i * q.i + q.j * q.j));
-    e.beta = asinf(2 * (q.w * q.j - q.i * q.k);
-    e.gamma = atan2f(2 * (q.w * q.k + q.i * q.j), 2 * (q.j * q.j + q.k * q.k));
+    e.alpha = atan2(2 * (q.w * q.i + q.j * q.k), 1 - 2 * (q.i * q.i + q.j * q.j));
+    e.beta = qrot_asin(2 * (q.w * q.j - q.i * q.k));
+    e.gamma = atan2(2 * (q.w * q.k + q.i * q.j), 2 * (q.j * q.j + q.k * q.k));
 
     return e;
 }
