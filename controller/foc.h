cb541c1b0104d764b01ac9def96ab402cc192038
luoqi <logeexpluoqi@outlook.com>
2023-06-17 21:53:15 +0800

    files(controller): add foc controller files (not finish)

---
+++ controller/foc.h
@@ -0,0 +1,23 @@
+/*
+ * @Author: luoqi 
+ * @Date: 2023-05-22 01:14:18 
+ * @Last Modified by: luoqi
+ * @Last Modified time: 2023-05-22 01:15:37
+ */
+
+#ifndef _FOC_H
+#define _FOC_H
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+typedef struct foc
+{
+    
+} FocObj;
+
+
+#ifdef __cpluscplus
+}
+#endif
+#endif
